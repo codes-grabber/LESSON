c585be7b944773246b54d8933525055f9959f93b
samarastark <leninart@mail.ru>
2021-10-04 02:04:14 +0400

    Проверка

--- src/LESSON.java
+++ src/LESSON.java
@@ -1,5 +1,16 @@
 public class LESSON {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        telo();
+        byte myByte = 8; //целое число 8 бит  -128 до 127
+        short myShort = 16; //целое число 2 байта (16 бит) -32768 до 32767
+        int myInt = 32; //целое число 4 байта (32 бит) использовать по умолчанию
+        long myLong = 212132121231L; //целое число 8 байт (64 бит)
+        float myFloat = 64.23f; //число с плав запятой -  4 байт
+        double myDouble = 64.121312132; //число с плавающей запятой - 8 байт
+        char myChar = '♦'; //символьный тип данных - 16 бит (2 байта)
+        boolean myBoolean = true; //логический тип данных
+        }
+    public static void telo(){
+        System.out.println("TELO");
     }
 }
