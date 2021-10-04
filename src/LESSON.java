ce035952be9da5ba72b5900c8b34158910e89b21
samarastark <leninart@mail.ru>
2021-10-04 14:40:36 +0400

    Проверка

--- src/LESSON.java
+++ src/LESSON.java
@@ -4,6 +4,8 @@ public class LESSON {
         checkSumSign();
         printColor();
         compareNumbers();
+        System.out.println(myMethod());
+        System.out.println(myAnotherMethod());
 //        byte myByte = 8; //целое число 8 бит  -128 до 127
 //        short myShort = 16; //целое число 2 байта (16 бит) -32768 до 32767
 //        int myInt = 32; //целое число 4 байта (32 бит) использовать по умолчанию
@@ -12,6 +14,25 @@ public class LESSON {
 //        double myDouble = 64.121312132; //число с плавающей запятой - 8 байт
 //        char myChar = '♦'; //символьный тип данных - 16 бит (2 байта)
 //        boolean myBoolean = true; //логический тип данных
+        int g = 1;
+        switch(g){
+            case 1:
+                System.out.println("Это 1");
+                break;
+            case 5:
+                System.out.println("This 5");
+                break;
+        }
+    }
+
+
+    public static int myMethod(){
+        int a = 5, b=3;
+        int c = a + b;
+        return c;
+    }
+    public static char myAnotherMethod(){
+        return 'c';
     }
 
     public static void printThreeWords() {
