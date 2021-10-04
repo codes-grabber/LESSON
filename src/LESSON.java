d0877d3858ece0cc030ed91e61b479c64827903c
samarastark <leninart@mail.ru>
2021-10-04 17:16:20 +0400

    Сделал третье задание

--- src/LESSON.java
+++ src/LESSON.java
@@ -1,24 +1,46 @@
 public class LESSON {
     public static void main(String[] args) {
-        System.out.println(myMethod(1,15));
+        System.out.println(myMethod(1, 15));
         System.out.println(myAnotherMethod(-4));
         System.out.println(myThirdMethod(4));
+        myFourMethod("Hi", 5);
 
     }
-    public static boolean myMethod(int a, int b){
 
-        if((a+b) >= 10 && (a+b) <= 20){
+    public static boolean myMethod(int a, int b) {
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
             return true;
         } else return false;
     }
-    public static String myAnotherMethod(int c){
-        if (c >= 0){
+
+    public static String myAnotherMethod(int c) {
+        if (c >= 0) {
             return "Положительное";
         } else return "Отрицательное";
     }
-    public static boolean myThirdMethod(int d){
-        if (d >= 0){
+
+    public static boolean myThirdMethod(int d) {
+        if (d >= 0) {
             return true;
         } else return false;
+    }
+
+    public static void/*String*/ myFourMethod(String a, int b) {
+//        int i = 0;
+//        String g = " ";
+//        while (i < b) {
+//            g += "\n" + a;
+//            i++;
+//        }
+//        return g;
+        int i = 0;
+        while (i != b){
+            System.out.println(a);
+            i++;
+        if (i == b) {
+            break;
+        }
+        }
     }
 }
