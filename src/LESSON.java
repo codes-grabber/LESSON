d3f86f4729c31df945e8650d9db9db8ffd268244
samarastark <leninart@mail.ru>
2021-10-04 15:23:44 +0400

    Сделал третье задание

--- src/LESSON.java
+++ src/LESSON.java
@@ -2,18 +2,23 @@ public class LESSON {
     public static void main(String[] args) {
         System.out.println(myMethod(1,15));
         System.out.println(myAnotherMethod(-4));
+        System.out.println(myThirdMethod(4));
 
     }
     public static boolean myMethod(int a, int b){
-        int c;
-        if(c >= 10 && c <= 20){ //java: variable c might not have been initialized
-            return true;
 
+        if((a+b) >= 10 && (a+b) <= 20){
+            return true;
         } else return false;
     }
     public static String myAnotherMethod(int c){
         if (c >= 0){
             return "Положительное";
         } else return "Отрицательное";
+    }
+    public static boolean myThirdMethod(int d){
+        if (d >= 0){
+            return true;
+        } else return false;
     }
 }
